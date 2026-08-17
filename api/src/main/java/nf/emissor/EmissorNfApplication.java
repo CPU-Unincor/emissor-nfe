395feb555a9fb481dbee7bd4366ca561cc74723f
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2026-08-17 20:41:07 -0300

    feat(api): base da api com spring boot

---
+++ api/src/main/java/nf/emissor/EmissorNfApplication.java
@@ -0,0 +1,13 @@
+package nf.emissor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissorNfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissorNfApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/nf/emissor/EmissorNfApplicationTests.java
@@ -0,0 +1,13 @@
+package nf.emissor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissorNfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
